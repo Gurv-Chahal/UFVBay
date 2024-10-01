b87a179b36a4f926fe7f36d0cd075d1969c236f3
WilsoAgya <wilsonagyapong16@gmail.com>
2024-09-30 19:23:21 -0700

    created login-signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,20 @@
-import { useState } from 'react'
-import Home from './pages/Home.jsx';
-import Account from './pages/Account.jsx';
-import Auth from './pages/Auth.jsx';
-import { Routes, Route, BrowserRouter} from 'react-router-dom'
+import { useState } from "react";
+import Home from "./pages/Home.jsx";
+import Account from "./pages/Account.jsx";
+import Auth from "./pages/Auth.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <BrowserRouter>
       <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/login' element={<Auth />} />
-      <Route path='/account' element = {<Account />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Auth />} />
+        <Route path="/account" element={<Account />} />
       </Routes>
-    
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
