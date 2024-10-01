1176ed358a3d2558d166a8f82941194e8dbe64c2
Gurv-Chahal <gurveer.chahal15@icloud.com>
2024-09-30 19:23:28 -0700

    refactoring names

--- server/demo/src/main/java/mainfiles/Main/boot.java
+++ server/demo/src/main/java/mainfiles/Main/boot.java
@@ -1,13 +1,13 @@
-package mainfiles.demo;
+package mainfiles.Main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class boot {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(boot.class, args);
 	}
 
 }
