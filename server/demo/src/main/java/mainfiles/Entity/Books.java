ca86f9f6a30217a5a43b181ff4dd6e5557992448
Gurv-Chahal <gurveer.chahal15@icloud.com>
2024-10-13 14:47:13 -0700

    attempt 2 at comitting new entities

---
+++ server/demo/src/main/java/mainfiles/Entity/Books.java
@@ -0,0 +1,25 @@
+package mainfiles.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Book")
+public class Books {
+  
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long bookId;
+
+
+  @Column(nullable = false)
+  private String name;
+
+
+  @Column(nullable = false)
+  private String author;
+
+  @Column
+  private String classes;
+  
+}
