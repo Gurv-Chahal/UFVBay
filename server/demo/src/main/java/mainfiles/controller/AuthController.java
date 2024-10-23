8418a3a950c7ed70a69f872a7d5c37af96438516
Gurv-Chahal <gurveer.chahal15@icloud.com>
2024-10-23 14:15:00 -0700

    outline code for login/registration restapi, still need to add further implementation

--- server/demo/src/main/java/mainfiles/controller/AuthController.java
+++ server/demo/src/main/java/mainfiles/controller/AuthController.java
@@ -2,6 +2,10 @@ package mainfiles.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+import mainfiles.service.AuthService;
+import mainfiles.dto.*;
 
 
 /* 
@@ -10,10 +14,24 @@ this is why its marked @RestController
 */
 
 
+// combines @controller and @responsebody so you dont have to use @responsebody on every
+// method. It also handles to http requests so we can do things like @GetMapping
 @RestController
+// its a base api you have to access first for example UFVBay.com/auth/login
 @RequestMapping("/auth")
 public class AuthController {
 
-  
+  private AuthService authservice;
+
+
+  // Build Register RESTAPI
+  public ResponseEntity<String> register(RegisterDTO registerDTO) {
+    return null;
+  }
+
+  // Build Login RESTAPI
+  public ResponseEntity<String> login(LoginDTO loginDTO) {
+    return null;
+  }
 
 }
